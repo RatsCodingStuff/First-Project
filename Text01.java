9e71cd9a4fffedbb7ee6157b8d618793a4ac05b6
RatsCodingStuff <aball50@student.cccs.edu>
2026-09-02 17:35:30 -0600

    Add files via upload

---
+++ Text01.java
@@ -0,0 +1,10 @@
+void main() {
+        System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+}
---
+++ Text02.java
@@ -0,0 +1,6 @@
+void main() {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+    }
